7c64c285643a22b8da91886cb5ae4ce89e95c568
Jabbor <94156694+Jabbor17@users.noreply.github.com>
2022-01-05 11:29:10 +0500

    second init

--- app/src/main/java/com/masterandroid/videoplayer/MainActivity.kt
+++ app/src/main/java/com/masterandroid/videoplayer/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.masterandroid.videoplayer
 
+import android.R.attr
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import net.alhazmy13.mediapicker.Image.ImagePicker
+import net.alhazmy13.mediapicker.Video.VideoPicker
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       initViews()
     }
+    fun initViews(){
+        val playVideo = findViewById<Button>(R.id.btn_play_video)
+
+        playVideo.setOnClickListener {
+            pickVideo()
+        }
+    }
+    fun pickVideo(){
+        val videoPicker = VideoPicker.Builder(this@MainActivity)
+            .mode(VideoPicker.Mode.GALLERY)
+            .directory(VideoPicker.Directory.DEFAULT)
+            .extension(VideoPicker.Extension.MP4)
+            .enableDebuggingMode(true)
+
+        videoPicker.build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == VideoPicker.VIDEO_PICKER_REQUEST_CODE && resultCode == RESULT_OK){
+            val videoPaths = data!!.getStringArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH)
+
+        }
+    }
+
 }
